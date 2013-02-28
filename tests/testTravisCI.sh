3e105d9a01dad7358abafcfa21f4e53831fe5418
Dale Henrichs <dhenrich@vmware.com>
2013-02-28 11:16:12 -0800

    slim down for shell script debugging ...

--- tests/testTravisCI.sh
+++ tests/testTravisCI.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -x
-#   - echo "running: $PROJECT_HOME/tests/$TRAVIS_SCRIPT"
-   - ./build.sh -i $ST $BUILD_ARG -f "$PROJECT_HOME/tests/$TRAVIS_SCRIPT" -o travisCI
-   - cd "${BUILD_PATH}/travisCI/"
-   - ls -altr *
-   - $BUILDER_CI_HOME/buildImageErrorCheck.sh
-#   - cat TravisTranscript.txt
-   - $BUILDER_CI_HOME/buildTravisStatusCheck.sh
+./build.sh -i $ST $BUILD_ARG -f "$PROJECT_HOME/tests/$TRAVIS_SCRIPT" -o travisCI
+cd "${BUILD_PATH}/travisCI/"
+ls -altr *
+$BUILDER_CI_HOME/buildImageErrorCheck.sh
+$BUILDER_CI_HOME/buildTravisStatusCheck.sh
 
