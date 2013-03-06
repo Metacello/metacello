e1e1930c01de07cad03cfbed0a7f4acfdc05a2f1
Dale Henrichs <dhenrich@vmware.com>
2013-03-06 07:37:06 -0800

    safe to remove ls? fix syntax error

--- tests/testTravisCI.sh
+++ tests/testTravisCI.sh
@@ -5,11 +5,7 @@
 
 $BUILDER_CI_HOME/testTravisCI.sh
 if [[ $? != 0 ]] ; then 
-  ls -altr *
-  ls -altr */*
   cat builds/travisCI/TravisTranscript.txt
   exit 1
 fi
-ls -altr *
-ls -altr */*
 cat builds/travisCI/TravisTranscript.txt
