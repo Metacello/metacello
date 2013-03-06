e2b541f3a4fbb89cb98a685657a1cb86fbca08b3
Dale Henrichs <dhenrich@vmware.com>
2013-03-06 08:08:33 -0800

    half a chance that I got it right this time?

--- tests/testTravisCI.sh
+++ tests/testTravisCI.sh
@@ -3,9 +3,9 @@
 # wrap builderCI script for a little debugging action
 #
 
-ls -l builds/travisCI/TravisTranscript.txt
 $BUILDER_CI_HOME/testTravisCI.sh
 if [[ $? != 0 ]] ; then 
+  cat builds/travisCI/TravisTranscript.txt
   exit 1
 fi
 cat builds/travisCI/TravisTranscript.txt
