471bdcde6f7376c84b9c016e09250b811b1d9cff
Dale Henrichs <dhenrich@vmware.com>
2013-03-06 15:34:50 -0800

    looks like gemstone issues left (builderCI bug) ... use standard travisTest.sh with -verbose flag

--- tests/testTravisCI.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-./build.sh -i $ST $BUILD_ARG -f "$PROJECT_HOME/tests/$TRAVIS_SCRIPT" -o travisCI
-cd "${BUILD_PATH}/travisCI/"
-ls -altr *
-$BUILDER_CI_HOME/buildImageErrorCheck.sh
-$BUILDER_CI_HOME/buildTravisStatusCheck.sh
-
